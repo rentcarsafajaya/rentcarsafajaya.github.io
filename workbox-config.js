0e88439d782a0bdc6e2558572e590888ef0b3f23
rentcarsafajaya <60593914+rentcarsafajaya@users.noreply.github.com>
2020-02-14 14:59:09 +0700

    tel:+
    tel:+

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "_site/",
   "globPatterns": [
-    "**/*.{html,eot,svg,ttf,woff,woff2,ico,xml,jpg,webp,png,json,txt}"
+    "**/*.{html,eot,svg,ttf,woff,woff2,ico,xml,jpg,webp,png,json,txt,js}"
   ],
   "swDest": "_site/sw.js"
 };
